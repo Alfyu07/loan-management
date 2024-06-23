d3bbda6f22ee4289844db3b9e933ef706180cdc8
Wahyu Alfandi <w.alfa433@gmail.com>
2024-06-23 20:49:01 +0800

    chore: initial commit, add .gitignore to project

---
+++ Loan Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loan Management
+//
+//  Created by Wahyu Alfandi on 23/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
